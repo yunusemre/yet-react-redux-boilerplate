d3723b4b18c752f426f25fcd334f3b81db49f430
Yunus Emre Tatar <yunusemre.tatar@kolaygelsin.com>
2022-09-01 10:02:10 +0300

    added form example

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -6,17 +6,40 @@ import { RootState } from '../../store/configureStore';
 const Home = () => {
   const appState: any = useSelector((state: RootState) => state.app);
   const [form, setForm] = useState({});
+  const validate = (values: any) => {
+    const errors: any = {};
+    if (!values.firstName) {
+      errors.firstName = 'Required';
+    } else if (values.firstName.length > 15) {
+      errors.firstName = 'Must be 15 characters or less';
+    }
+
+    if (!values.lastName) {
+      errors.lastName = 'Required';
+    } else if (values.lastName.length > 20) {
+      errors.lastName = 'Must be 20 characters or less';
+    }
+
+    if (!values.email) {
+      errors.email = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+      errors.email = 'Invalid email address';
+    }
+
+    return errors;
+  };
+
   const formik = useFormik({
     initialValues: {
       firstName: '',
       lastName: '',
       email: '',
     },
-    onSubmit: (values) => {
+    validate,
+    onSubmit: (values: any) => {
       setForm(values);
     },
   });
-
   return (
     <>
       <form onSubmit={formik.handleSubmit}>
@@ -28,6 +51,7 @@ const Home = () => {
           onChange={formik.handleChange}
           value={formik.values.firstName}
         />
+        {formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
 
         <label htmlFor="lastName">Last Name</label>
         <input
@@ -37,6 +61,7 @@ const Home = () => {
           onChange={formik.handleChange}
           value={formik.values.lastName}
         />
+        {formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
 
         <label htmlFor="email">Email Address</label>
         <input
@@ -46,6 +71,7 @@ const Home = () => {
           onChange={formik.handleChange}
           value={formik.values.email}
         />
+        {formik.errors.email ? <div>{formik.errors.email}</div> : null}
 
         <button type="submit">Submit</button>
       </form>
