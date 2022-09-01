3732b2e8f184487c44d6926219bfe45851411884
Yunus Emre Tatar <yunusemre.tatar@kolaygelsin.com>
2022-09-01 10:23:40 +0300

    added form example

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,91 +1,57 @@
+import { Button, TextField } from '@mui/material';
+import { Box } from '@mui/system';
 import { useFormik } from 'formik';
-import { useState } from 'react';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../store/configureStore';
+import * as yup from 'yup';
 
 const Home = () => {
-  const appState: any = useSelector((state: RootState) => state.app);
-  const [form, setForm] = useState({});
-  const validate = (values: any) => {
-    const errors: any = {};
-    if (!values.firstName) {
-      errors.firstName = 'Required';
-    } else if (values.firstName.length > 15) {
-      errors.firstName = 'Must be 15 characters or less';
-    }
-
-    if (!values.lastName) {
-      errors.lastName = 'Required';
-    } else if (values.lastName.length > 20) {
-      errors.lastName = 'Must be 20 characters or less';
-    }
-
-    if (!values.email) {
-      errors.email = 'Required';
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-      errors.email = 'Invalid email address';
-    }
-
-    return errors;
-  };
+  const validationSchema = yup.object({
+    email: yup.string().email('Enter a valid email').required('Email is required'),
+    password: yup
+      .string()
+      .min(8, 'Password should be of minimum 8 characters length')
+      .required('Password is required'),
+  });
 
   const formik = useFormik({
     initialValues: {
-      firstName: '',
-      lastName: '',
-      email: '',
+      email: 'foobar@example.com',
+      password: 'foobar',
     },
-    validate,
-    onSubmit: (values: any) => {
-      setForm(values);
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
     },
   });
+
   return (
-    <>
+    <Box>
       <form onSubmit={formik.handleSubmit}>
-        <label htmlFor="firstName">First Name</label>
-        <input
-          id="firstName"
-          name="firstName"
-          type="text"
-          onChange={formik.handleChange}
-          value={formik.values.firstName}
-        />
-        {formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
-
-        <label htmlFor="lastName">Last Name</label>
-        <input
-          id="lastName"
-          name="lastName"
-          type="text"
-          onChange={formik.handleChange}
-          value={formik.values.lastName}
-        />
-        {formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
-
-        <label htmlFor="email">Email Address</label>
-        <input
+        <TextField
+          fullWidth
           id="email"
           name="email"
-          type="email"
-          onChange={formik.handleChange}
+          label="Email"
           value={formik.values.email}
+          onChange={formik.handleChange}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
         />
-        {formik.errors.email ? <div>{formik.errors.email}</div> : null}
-
-        <button type="submit">Submit</button>
+        <TextField
+          fullWidth
+          id="password"
+          name="password"
+          label="Password"
+          type="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          error={formik.touched.password && Boolean(formik.errors.password)}
+          helperText={formik.touched.password && formik.errors.password}
+        />
+        <Button color="primary" variant="contained" fullWidth type="submit">
+          Submit
+        </Button>
       </form>
-      <pre>{JSON.stringify(form, null, 2)}</pre>
-      <ul>
-        {appState.todoList.result.map((result: any) => (
-          <li key={result.name}>
-            <p>Name: {result.name}</p>
-            <p>E-mail: {result.email}</p>
-            <p>Website: {result.website}</p>
-          </li>
-        ))}
-      </ul>
-    </>
+    </Box>
   );
 };
 
