1591d4ac1b5006c5aeecf62aa3244c114f4b145d
Yunus Emre Tatar <yunusemre.tatar@kolaygelsin.com>
2022-09-01 10:00:24 +0300

    added form example

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,11 +1,55 @@
+import { useFormik } from 'formik';
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store/configureStore';
 
 const Home = () => {
   const appState: any = useSelector((state: RootState) => state.app);
+  const [form, setForm] = useState({});
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+    },
+    onSubmit: (values) => {
+      setForm(values);
+    },
+  });
 
   return (
     <>
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="firstName">First Name</label>
+        <input
+          id="firstName"
+          name="firstName"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.firstName}
+        />
+
+        <label htmlFor="lastName">Last Name</label>
+        <input
+          id="lastName"
+          name="lastName"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.lastName}
+        />
+
+        <label htmlFor="email">Email Address</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+      <pre>{JSON.stringify(form, null, 2)}</pre>
       <ul>
         {appState.todoList.result.map((result: any) => (
           <li key={result.name}>
@@ -15,7 +59,6 @@ const Home = () => {
           </li>
         ))}
       </ul>
-      Total Count {appState.todoList.totalCount}
     </>
   );
 };
