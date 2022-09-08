2a9248c1c62d5134fd59911da115239b63fbfeb8
Yunus Emre Tatar <yunusemre.tatar@kolaygelsin.com>
2022-09-08 17:04:46 +0300

    menu collapse fixed

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,10 +1,12 @@
+import { useAppSelector } from '../store/hooks';
 import { Sidebar } from './sidebar';
 
 const Layout = ({ children }: any): any => {
+  const menuState = useAppSelector((state) => state.app.menuOpen);
   return (
     <div className="wrapper d-flex">
       <Sidebar />
-      <div className="page-container">{children}</div>
+      <div className={`page-container${menuState ? ' open' : ''}`}>{children}</div>
     </div>
   );
 };
