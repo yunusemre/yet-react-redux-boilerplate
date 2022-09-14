63859a293e02a03ccba68f28835229e07abbf693
Yunus Emre Tatar <yunusemre.tatar@kolaygelsin.com>
2022-09-14 17:19:49 +0300

    remove loader

--- src/components/Loader/loader.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentMeta, ComponentStory } from '@storybook/react';
-import Loader from '.';
-
-export default {
-  title: 'Loader',
-  component: Loader,
-} as ComponentMeta<typeof Loader>;
-
-export const Primary: ComponentStory<typeof Loader> = () => <Loader />;
