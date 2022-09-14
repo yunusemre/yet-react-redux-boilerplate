1d15fbc309e7e867bb0b3226c1fc10f9ba2713d7
Yunus Emre Tatar <yunusemre.tatar@kolaygelsin.com>
2022-09-14 10:42:43 +0300

    updated template

--- src/router.tsx
+++ src/router.tsx
@@ -4,7 +4,6 @@ import { Navigate, useRoutes } from 'react-router-dom';
 const Layout = lazy(() => import('./layout'));
 const PageLayout = lazy(() => import('./layout/pages/page-layout'));
 const Home = lazy(() => import('./views/home'));
-const ShipmentSave = lazy(() => import('./views/shipment-operations/shipment-save'));
 const LoginPage = lazy(() => import('./layout/pages/login'));
 const ForgotPasswordPage = lazy(() => import('./layout/pages/forgot-password'));
 const Page404 = lazy(() => import('./layout/pages/404-page'));
@@ -18,16 +17,6 @@ const ProjectRouter = (): any => {
         {
           path: '/',
           element: <Home />,
-        },
-        {
-          path: '/shipment-operations',
-          element: <PageLayout />,
-          children: [
-            {
-              path: 'saved',
-              element: <ShipmentSave />,
-            },
-          ],
         },
         {
           path: '/profile',
