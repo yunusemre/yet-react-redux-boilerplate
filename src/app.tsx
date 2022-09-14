27ef458b2fb6517486c19518abe747cc2bd7b65e
Yunus Emre Tatar <yunusemre.tatar@kolaygelsin.com>
2022-09-14 17:26:38 +0300

    updated app

--- src/app.tsx
+++ src/app.tsx
@@ -2,18 +2,10 @@ import {Suspense} from 'react';
 import Loader from './components/loader';
 import ProjectRouter from './router';
 
-const App = (): any => {
-  // const dispatch: any = useAppDispatch();
-
-  // useEffect(() => {
-  //   dispatch(fetchUser());
-  // }, []);
-
-  return (
-    <Suspense fallback={<Loader />}>
-      <ProjectRouter />
-    </Suspense>
-  );
-};
+const App = (): any => (
+  <Suspense fallback={<Loader />}>
+    <ProjectRouter />
+  </Suspense>
+);
 
 export default App;
