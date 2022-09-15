4e31af9492aeea379e8469f7daf45ebcc84390b7
Yunus Emre Tatar <yunusemre.tatar@kolaygelsin.com>
2022-09-15 10:26:19 +0300

    fixed mobile

--- src/app.tsx
+++ src/app.tsx
@@ -2,10 +2,15 @@ import { Suspense } from 'react';
 import Loader from './components/loader';
 import ProjectRouter from './router';
 
-const App = (): any => (
-  <Suspense fallback={<Loader />}>
-    <ProjectRouter />
-  </Suspense>
-);
+const App = (): any => {
+  const vh: any = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+  return (
+    <Suspense fallback={<Loader />}>
+      <ProjectRouter />
+    </Suspense>
+  );
+};
 
 export default App;
